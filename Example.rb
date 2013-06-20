da15c0e14b846173053d53da92fe044b354c5ec1
Gero <gerbihc@linuxmint.linuxmint-domain>
2013-06-19 22:24:48 -0500

    Gerbihc

--- Example.rb
+++ Example.rb
@@ -1,4 +1,13 @@
-Class Hola
+class BookInStock
+	  attr_accessor :isbn, :Price
+ 	 def initialize (isbn, Price)
+   	
+		 @isbn= isbN
+   	 	@Price= Price
+  	 end
 
-	puts = "Holaa Zombie"
-:
+  
+ 	 def to
+   		 "ISBN: #{@isbn}, Price: #{@Price}
+ 	 end
+end 
